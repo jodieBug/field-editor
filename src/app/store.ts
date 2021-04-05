b43659777f4a969f0e0c8575fb50f2531abbdf8a
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 14:31:28 +0100

    Add fieldEditor reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import fieldEditorReducer from "../components/fieldEditor/fieldEditorSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    fieldEditor: fieldEditorReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
