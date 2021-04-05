64feea69809d9a43e8179e93b8d90de6b2981a0e
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 16:06:28 +0100

    Remove unused variable

--- src/components/fieldEditor/FieldEditor.test.tsx
+++ src/components/fieldEditor/FieldEditor.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import userEvent from "@testing-library/user-event";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
 import { FieldEditor } from "./FieldEditor";
