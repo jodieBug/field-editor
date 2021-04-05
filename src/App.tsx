1c3fe6b6ddf5a7e7b49831af9ba8396f9612feea
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 14:35:30 +0100

    Add FieldEditor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { FieldEditor } from "./components/fieldEditor/FieldEditor";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FieldEditor />
+    </div>
+  );
 }
 
 export default App;
