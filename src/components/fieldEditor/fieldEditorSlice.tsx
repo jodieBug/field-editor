0807a619a6df7efca399b3e183930cb2a3c2406e
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 13:38:43 +0100

    Create slice and setup initial values

--- src/components/fieldEditor/fieldEditorSlice.tsx
+++ src/components/fieldEditor/fieldEditorSlice.tsx
@@ -1,4 +1,5 @@
 import * as data from "../../response.json";
+import { createSlice } from "@reduxjs/toolkit";
 
 interface FieldEditorState {
   fieldDefinitions: string[];
@@ -11,3 +12,11 @@ const initialState: FieldEditorState = {
   title: "",
   selectedFields: [],
 };
+
+export const fieldsEditorSlice = createSlice({
+  name: "FieldEditor",
+  initialState,
+  reducers: {
+    
+  }
+})
