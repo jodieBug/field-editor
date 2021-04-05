16f065acec057aa1c927d3b4c7d7d8cf5bb96e94
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 16:09:55 +0100

    Order imports

--- src/components/fieldEditor/FieldEditor.test.tsx
+++ src/components/fieldEditor/FieldEditor.test.tsx
@@ -1,8 +1,8 @@
 import userEvent from "@testing-library/user-event";
 import { render, screen, within } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "../../app/store";
 import { FieldEditor } from "./FieldEditor";
+import { store } from "../../app/store";
 
 let useButton: HTMLElement;
 let removeButton: HTMLElement;
