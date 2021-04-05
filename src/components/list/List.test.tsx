7d1fac4c5d45743201d3e5da9842a8620114f7c9
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 15:13:10 +0100

    Check that the list component renders a title

--- src/components/list/List.test.tsx
+++ src/components/list/List.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { List } from "./List";
 
 const pokemon = [
@@ -19,5 +19,9 @@ describe("list component", () => {
         listItems={pokemon}
       />
     );
+  });
+
+  it("renders a title", () => {
+    expect(screen.getByText(/pokemon/i)).toBeInTheDocument();
   });
 });
