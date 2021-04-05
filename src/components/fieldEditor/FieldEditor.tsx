f2eb30c9616487833644bfdaf857baad33d43a97
Jodie Ide <jodie@computech-it.co.uk>
2021-04-05 14:17:44 +0100

    Create field editor component with title

--- src/components/fieldEditor/FieldEditor.tsx
+++ src/components/fieldEditor/FieldEditor.tsx
@@ -7,3 +7,15 @@ import {
 } from "./fieldEditorSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import styles from "./FieldEditor.module.css";
+
+export const FieldEditor: React.FunctionComponent = () => {
+  const { fieldDefinitions, selectedFields, title } = useAppSelector(
+    selectFieldEditor
+  );
+
+  return (
+    <div className={styles.fieldEditor}>
+      <div className={styles.title}>{title}</div>
+    </div>
+  );
+};
